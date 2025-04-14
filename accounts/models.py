1a7c2ec44ffa4c02ece947b37317c02afa729c94
Pritom Karmokar <pritomkarmokar99@gmail.com>
2025-04-15 02:50:16 +0600

    feat: password reset model

--- accounts/models.py
+++ accounts/models.py
@@ -50,3 +50,6 @@ class PasswordReset(models.Model):
     email = models.EmailField()
     token = models.CharField(max_length=100)
     created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.email
