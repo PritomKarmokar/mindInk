2f9c005c78e00ec79ad8fdb7d164f4fa002ac868
Pritom Karmokar <pritomkarmokar99@gmail.com>
2025-04-20 01:09:13 +0600

    feat: working on notes api

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+
+class NotesListCreateAPIView(APIView):
+    pass
