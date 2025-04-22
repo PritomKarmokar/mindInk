3724f4377546bdab8a6cbbb1a1038dd976c748d4
Pritom Karmokar <pritomkarmokar99@gmail.com>
2025-04-22 21:58:59 +0600

    feat: unit tests for note create

--- notes/tests.py
+++ notes/tests.py
@@ -9,7 +9,7 @@ from .serializers import NoteSerializer
 
 class BaseViewTest(APITestCase):
     def setUp(self):
-        # self.url = reverse('note-list')
+        self.url = reverse("notes-create-or-list")
         self.authenticate_user()
 
     def authenticate_user(self):
@@ -21,14 +21,23 @@ class BaseViewTest(APITestCase):
                 "password": "test123",
             },
         )
-        print(signup_response.content)
+        print(signup_response.data)
 
         login_response = self.client.post(
             reverse("login"),
             data={"email": "testuser@gmail.com", "password": "test123"},
         )
-        print(login_response.content)
+        print(login_response.data)
 
         token = login_response.data["access_token"]
 
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+
+    def test_create_note(self):
+        new_note = {
+            "title": "Test note",
+            "content": "Test content",
+        }
+        response = self.client.post(self.url, data=new_note)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Note.objects.count(), 1)
